e4ae6a50f7bb14bf09251b37d4c9a785492439bf
Nekopunch <Nekopunch2482@yandex.ru>
2025-02-18 18:15:14 +0300

    feat: implement mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,15 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects
+    .map(Object.entries)
+    .flat()
+    .reduce(
+      (acc, [key, value]) =>
+        Object.assign(acc, { [key]: (acc[key] || 0) + value }),
+      {}
+    );
 }
 
 /**
